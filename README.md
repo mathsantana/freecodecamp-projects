ff5a3241b21f150daa382ae05e645f673616a366
mathsantana <matheus.santana@ccc.ufcg.edu.br>
2021-02-22 02:31:38 -0300

    initial commit and test if gh-pages works

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # freecodecamp-projects
 Projects from Freecodecamp Curriculum.
+
+</br>
+
+<table>
+  <thead>
+  <tr>
+    <th>Certification Name</th>
+    <th>Projects</th>
+    <th>Status</th>
+    <th>Certification</th>
+  </tr>
+  </thead>
+  <tbody>
+  <tr>
+    <td rowspan="5">Responsive Web Design</td>
+    <td>Tribute Page</td>
+    <td style="color:yellow">IN PROGRESS</td>
+    <td rowspan="5"> <img src="./assets/incomplete-certificate.png" alt="Missing certificate"></td>
+  </tr>
+  <tr>
+    <td>Survey Form</td>
+    <td style="color:red">NOT DONE</td>
+  </tr>
+  <tr>
+    <td>Product Landing Page</td>
+    <td style="color:red">NOT DONE</td>
+  </tr>
+  <tr>
+    <td>Technical Documentation Page</td>
+    <td style="color:red">NOT DONE</td>
+  </tr>
+  <tr>
+    <td>Personal Portfolio Webpage</td>
+    <td style="color:red">NOT DONE</td>
+  </tr>
+
+  </tbody>
+</table>
---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const fs = require("fs");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync");
+const autoPrefixer = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin");
+// const uglufy = require('uglify')
+// const postcss    = require('gulp-postcss')
+// const sourcemaps = require('gulp-sourcemaps')
+//var cleanCSS = require('gulp-clean-css');
+// var concat = require('gulp-concat');
+// var bless = require('gulp-bless');
+
+const DEST_PATH = "dist/responsive-web-design";
+
+const html = () => {
+  return gulp
+    .src("responsive-web-design/**/**/*.html")
+    .pipe(gulp.dest(DEST_PATH));
+};
+
+const styles = () => {
+  return gulp
+    .src("responsive-web-design/**/src/scss/**/*.scss")
+    .pipe(
+      sass({
+        errLogToConsole: true,
+        outputStyle: "compressed", // compiles SASS to CSS
+      })
+    )
+    .pipe(
+      autoPrefixer({
+        cascade: false,
+      })
+    )
+    .pipe(gulp.dest(DEST_PATH))
+    .pipe(browserSync.stream()); // tells task which directory to output compiled CSS [optional]
+};
+
+const scripts = () => {
+  return gulp
+    .src([
+      // The order that you list the files in this array IS IMPORTANT!!
+      "responsive-web-design/**/src/js/**/*.js",
+    ])
+    .pipe(gulp.dest(DEST_PATH)); // tells task which directory to outputs uglified (minified) scripts.min.js
+};
+
+const images = () => {
+  return gulp
+    .src("responsive-web-design/**/src/assets/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest(DEST_PATH));
+};
+
+const watch = () => {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  gulp.watch("src/scss/**/*.scss", styles); // Watch the sass files for changes
+  gulp.watch("./*.html").on("change", browserSync.reload); // Watch the JS files for changes
+  gulp.watch("src/js/**/*.js", scripts).on("change", browserSync.reload); // Watch the JS files for changes
+  gulp.watch("src/assets/**/*", images).on("change", browserSync.reload); // Watch the image files for changes
+};
+
+exports.default = gulp.series(gulp.parallel(html, styles, images, scripts));
